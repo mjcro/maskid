7e3ebb3945896c7805185aa05f57d3a560e7be62
gotterdemarung <gotterdemarung@gmail.com>
2023-02-16 22:55:11 +0200

    :sparkles: Multiplication shift masker

--- src/main/java/org/github/mjcro/maskid/MultiplicationShift.java
+++ src/main/java/org/github/mjcro/maskid/MultiplicationShift.java
@@ -1,9 +1,21 @@
 package org.github.mjcro.maskid;
 
+/**
+ * Perform multiplication shift masking by formula:
+ * masked = original * multiplier + shift
+ * <p>
+ * Best result achieved when both multiplier and shift are prime numbers.
+ */
 public class MultiplicationShift implements Masker {
     private final long shift;
     private final int multiplier;
 
+    /**
+     * Constructs multiplication shift masker.
+     *
+     * @param shift      Shift offset.
+     * @param multiplier Multiplier.
+     */
     public MultiplicationShift(final long shift, final int multiplier) {
         this.shift = shift;
         this.multiplier = multiplier;
@@ -17,5 +29,10 @@ public class MultiplicationShift implements Masker {
     @Override
     public long unmask(final long value) {
         return (value - shift) / multiplier;
+    }
+
+    @Override
+    public String toString() {
+        return "{Multiplication " + multiplier + " shift " + shift + "}";
     }
 }
