47f13fc82c1cc35096f86c8b605fb0b5ea3315cb
Gotterdemarung <gotterdemarung@gmail.com>
2023-02-17 12:47:27 +0200

    :sparkles: Multiplication shift masker

---
+++ src/main/java/org/github/mjcro/maskid/MultiplicationShiftMasker.java
@@ -0,0 +1,88 @@
+package org.github.mjcro.maskid;
+
+import java.math.BigInteger;
+
+/**
+ * Perform multiplication shift masking by formula:
+ * masked = original * multiplier + shift.
+ * Sign of given value is never changed - positive remains positive and negative remains negative.
+ * <p>
+ * Best result achieved when both multiplier and shift are prime numbers.
+ */
+public class MultiplicationShiftMasker implements IntMasker, LongMasker {
+    private final BigInteger modLong = BigInteger.valueOf(Long.MAX_VALUE).add(BigInteger.ONE);
+    private final BigInteger modInt = BigInteger.valueOf(Integer.MAX_VALUE).add(BigInteger.ONE);
+    private final BigInteger shift;
+    private final BigInteger multiplier;
+
+    /**
+     * Constructs multiplication shift masker.
+     *
+     * @param shift      Shift offset.
+     * @param multiplier Multiplier.
+     */
+    public MultiplicationShiftMasker(final long shift, final long multiplier) {
+        if (shift < 0) {
+            throw new IllegalArgumentException("Shift should be positive, " + shift + " given");
+        }
+        if (multiplier < 0) {
+            throw new IllegalArgumentException("Multiplier should be positive, " + multiplier + " given");
+        }
+
+        this.shift = BigInteger.valueOf(shift);
+        this.multiplier = BigInteger.valueOf(multiplier);
+    }
+
+    private BigInteger mask(final BigInteger modulus, BigInteger bi) {
+        bi = bi.multiply(multiplier);
+        if (bi.signum() >= 0) {
+            bi = bi.add(shift);
+            return bi.mod(modulus);
+        } else {
+            bi = bi.subtract(shift);
+            return bi.negate().mod(modulus).negate();
+        }
+    }
+
+    @Override
+    public int maskInt(final int value) {
+        return mask(modInt, BigInteger.valueOf(value)).intValue();
+    }
+
+    @Override
+    public long maskLong(final long value) {
+        return mask(modLong, BigInteger.valueOf(value)).longValue();
+    }
+
+    private BigInteger unmask(final BigInteger modulus, BigInteger bi) {
+        if (bi.signum() >= 0) {
+            bi = bi.subtract(shift);
+            bi = bi.divide(multiplier);
+            if (bi.signum() < 0) {
+                bi = bi.add(modulus);
+            }
+        } else {
+            bi = bi.add(shift);
+            bi = bi.divide(multiplier);
+            if (bi.signum() >= 0) {
+                bi = bi.subtract(modulus);
+            }
+        }
+        return bi;
+    }
+
+    @Override
+    public int unmaskInt(final int value) {
+        return unmask(modInt, BigInteger.valueOf(value)).intValue();
+    }
+
+    @Override
+    public long unmaskLong(final long value) {
+        return unmask(modLong, BigInteger.valueOf(value)).longValue();
+    }
+
+    @Override
+    public String toString() {
+        return "{Multiplication " + multiplier + " shift " + shift + "}";
+    }
+}
---
+++ src/test/java/org/github/mjcro/maskid/MultiplicationShiftMaskerTest.java
@@ -0,0 +1,51 @@
+package org.github.mjcro.maskid;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class MultiplicationShiftMaskerTest {
+    @DataProvider
+    public Object[][] maskingLongDataProvider() {
+        return new Object[][]{
+                {0, 13},
+                {1, 18},
+                {-1, -18},
+                {12345, 61738},
+                {-12345, -61738},
+                {Long.MAX_VALUE - 1, 3},
+                {Long.MAX_VALUE, 8},
+                {Long.MIN_VALUE, -13},
+                {Long.MIN_VALUE + 1, -8},
+        };
+    }
+
+    @Test(dataProvider = "maskingLongDataProvider")
+    public void testMaskingLong(final long given, final long expected) {
+        LongMasker m = new MultiplicationShiftMasker(13, 5);
+        Assert.assertEquals(m.maskLong(given), expected);
+        Assert.assertEquals(m.unmaskLong(expected), given);
+    }
+
+    @DataProvider
+    public Object[][] maskingIntDataProvider() {
+        return new Object[][]{
+                {0, 13},
+                {1, 18},
+                {-1, -18},
+                {12345, 61738},
+                {-12345, -61738},
+                {Integer.MAX_VALUE - 1, 3},
+                {Integer.MAX_VALUE, 8},
+                {Integer.MIN_VALUE, -13},
+                {Integer.MIN_VALUE + 1, -8},
+        };
+    }
+
+    @Test(dataProvider = "maskingIntDataProvider")
+    public void testMaskingInt(final int given, final int expected) {
+        IntMasker m = new MultiplicationShiftMasker(13, 5);
+        Assert.assertEquals(m.maskInt(given), expected);
+        Assert.assertEquals(m.unmaskInt(expected), given);
+    }
+}
